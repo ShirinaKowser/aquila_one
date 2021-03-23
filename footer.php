db9c104851b99199b13213261eb6afbda39d9a5a
“Shirina <shirinakowser@gmail.com>
2021-03-23 22:43:23 +0000

    added all files

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php 
+ /* Footer Template
+ *
+ * @package Aquila
+  */?> 
+   <footer>Footer</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Main Template File
+ *@package aquila
+ */
+get_header( );
+?>
+
+ <content>Content</content>
+ <?php get_footer();
